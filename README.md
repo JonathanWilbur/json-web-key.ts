2c31fe23f5201da3cdb9d50b934da3fa62794f87
Jonathan M. Wilbur <jonathan@wilbur.space>
2020-09-19 13:22:02 -0400

    Initial commit
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

---
+++ README.md
@@ -0,0 +1,19 @@
+# OpenID Provider Metadata Schema and Types
+
+* Author: [Jonathan M. Wilbur](https://github.com/JonathanWilbur) <[jonathan@wilbur.space](mailto:jonathan@wilbur.space)>
+* Copyright Year: 2020
+* License: [MIT License](https://mit-license.org/)
+
+JSON Schema (both as a JavaScript Object and as JSON) and TypeScript
+interfaces for representing JSON Web Keys, as described
+[here](https://tools.ietf.org/html/rfc7517#section-4).
+
+## Recreate
+
+I am not going to implement scripts or automation for this, because it will
+probably not going to change very often.
+
+1. Compile `index.ts` to `index.js`: `tsc ./source/index.ts`.
+2. Convert `index.js` to `index.json`: `node -e 'console.log(JSON.stringify(require(\"./source/index.js\").default));' > .\source\index.json`.
+3. Convert `index.json` to `index.d.ts`: `node ./tools/compileInterfaces.js`.
+4. Just manually dereference `JSONWebKey` in `index.json` to create `index.deref.json`.
---
+++ source/index.ts
@@ -0,0 +1,112 @@
+import { JSONSchema6 } from "json-schema";
+
+const schema: JSONSchema6 = {
+    // $id: "",
+    $schema: "http://json-schema.org/draft-07/schema#",
+    title: "JSONWebKeySet",
+    description: "A set of JSON Web Keys, as defined in IETF RFC 7517.",
+    type: "object",
+    required: [
+        "keys",
+    ],
+    /**
+     * From IETF RFC 7517, Section 5:
+     * "Additional members can be present in the JWK Set; ..."
+     */
+    additionalProperties: true,
+    properties: {
+        keys: {
+            type: "array",
+            items: {
+                "$ref": "#/definitions/JSONWebKey"
+            },
+        },
+    },
+    definitions: {
+        JSONWebKey: {
+            title: "JSONWebKey",
+            description: "A single JSON Web Key.",
+            type: "object",
+            required: [
+                "kty",
+            ],
+            properties: {
+                kty: {
+                    type: "string",
+                    description:
+                    "The \"kty\" (key type) parameter identifies the "
+                    + "cryptographic algorithm family used with the key, such "
+                    + "as \"RSA\" or \"EC\".",
+                },
+                use: {
+                    type: "string",
+                    description:
+                    "The \"kty\" (key type) parameter identifies the cryptographic algorithm "
+                    + "family used with the key, such as \"RSA\" or \"EC\".",
+                },
+                key_ops: {
+                    type: "array",
+                    items: {
+                        type: "string",
+                    },
+                    description:
+                    "The \"key_ops\" (key operations) parameter identifies the operation(s) "
+                    + "for which the key is intended to be used.  The \"key_ops\" parameter is "
+                    + "intended for use cases in which public, private, or symmetric keys "
+                    + "may be present.",
+                },
+                alg: {
+                    type: "string",
+                    description:
+                    "The \"alg\" (algorithm) parameter identifies the algorithm intended for "
+                    + "use with the key.",
+                },
+                kid: {
+                    type: "string",
+                    description:
+                    "The \"kid\" (key ID) parameter is used to match a specific key. This "
+                    + "is used, for instance, to choose among a set of keys within a JWK Set "
+                    + "during key rollover.  The structure of the \"kid\" value is "
+                    + "unspecified.",
+                },
+                x5u: {
+                    type: "string",
+                    description:
+                    "The \"x5u\" (X.509 URL) parameter is a URI that refers to a "
+                    + "resource for an X.509 public key certificate or certificate chain.",
+                },
+                x5c: {
+                    type: "array",
+                    items: {
+                        type: "string",
+                    },
+                    description:
+                    "The \"x5c\" (X.509 certificate chain) parameter contains a chain of one "
+                    + "or more PKIX certificates [RFC5280].  The certificate chain is "
+                    + "represented as a JSON array of certificate value strings.",
+                },
+                x5t: {
+                    type: "string",
+                    description:
+                    "The \"x5t\" (X.509 certificate SHA-1 thumbprint) parameter is a "
+                    + "base64url-encoded SHA-1 thumbprint (a.k.a. digest) of the DER "
+                    + "encoding of an X.509 certificate.",
+                },
+                "x5t#S256": {
+                    type: "string",
+                    description:
+                    "The \"x5t#S256\" (X.509 certificate SHA-256 thumbprint) parameter is a "
+                    + "base64url-encoded SHA-256 thumbprint (a.k.a. digest) of the DER "
+                    + "encoding of an X.509 certificate.",
+                },
+            },
+            /**
+             * From IETF RFC 7517, Section 4:
+             * "Additional members can be present in the JWK."
+             */
+            additionalProperties: true,
+        },
+    },
+};
+
+export default schema;
